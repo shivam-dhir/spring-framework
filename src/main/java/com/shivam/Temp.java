70dd779d0ab1becbe5f8e3293ad227ebff2c61e5
shivam130502 <shivamdhir90@gmail.com>
2025-03-04 19:32:51 -0800

    Initial Commit

---
+++ src/main/java/com/shivam/Temp.java
@@ -0,0 +1,12 @@
+package com.shivam;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Temp {
+
+	public void doSomething() {
+		System.out.println("doing something...");
+	}
+	
+}
