72e521a37d9981a5183ae92b815eb7797d0017d3
shivam130502 <shivamdhir90@gmail.com>
2025-03-06 22:25:53 -0800

    update application config

--- src/main/java/com/shivam/SpringFrameworkApplication.java
+++ src/main/java/com/shivam/SpringFrameworkApplication.java
@@ -9,6 +9,8 @@ public class SpringFrameworkApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringFrameworkApplication.class, args); 	
+		
+		// getBean is a method of ApplicationContext interface which is used to get the bean of the class 
 		Temp temp = context.getBean(Temp.class);
 		temp.doSomething(); 	 
 	}
